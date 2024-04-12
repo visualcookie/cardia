31a12884919b302188921cc15affef0d8e57b9a8
visualcookie <hello@visualcookie.me>
2024-04-12 10:10:49 +0200

    chore: Remove export as a server action

--- src/actions/records.ts
+++ src/actions/records.ts
@@ -3,7 +3,6 @@
 import { revalidatePath } from 'next/cache'
 import { eq } from 'drizzle-orm'
 import { z } from 'zod'
-import writeXlsxFile, { Schema } from 'write-excel-file'
 import { newRecordSchema } from '@/components/NewRecordForm'
 import { db } from '@/db'
 import { records } from '@/db/schema'
@@ -44,69 +43,4 @@ export async function deleteRecord(recordId: string) {
   }
 
   revalidatePath('/app')
-}
-
-export async function exportRecords(prevState: any, formData: FormData) {
-  const userId = formData.get('userId') as string
-
-  try {
-    const dbRecords = await db.query.records.findMany({
-      where: eq(records.userId, userId),
-    })
-
-    if (dbRecords.length === 0) {
-      return {
-        title: 'Export failed',
-        message: 'We could not find any records to export.',
-      }
-    }
-
-    const schema: Schema<{
-      recordedAt: Date
-      systolic: number
-      diastolic: number
-      pulse: number
-      id: string
-      userId: string
-    }> = [
-      {
-        column: 'Recorded at',
-        type: Date,
-        format: 'yyyy-mm-dd hh:mm:ss',
-        value: (record) => record.recordedAt,
-      },
-      {
-        column: 'Systolic',
-        type: Number,
-        value: (record) => record.systolic,
-      },
-      {
-        column: 'Diastolic',
-        type: Number,
-        value: (record) => record.diastolic,
-      },
-      {
-        column: 'Pulse',
-        type: Number,
-        value: (record) => record.pulse,
-      },
-    ]
-
-    await writeXlsxFile(dbRecords, {
-      schema,
-      fileName: `records-${new Date().getTime()}.xlsx`,
-    })
-
-    console.table(dbRecords)
-
-    return {
-      title: 'Export successful',
-      message: 'Your records have been exported successfully.',
-    }
-  } catch (error) {
-    return {
-      title: 'Export failed',
-      message: 'Could not export records. Please try again.',
-    }
-  }
 }
